9851ec927130b800f304caf21fabfcf512edd0b5
Chuck Siebsen <csiebsen@yahoo.com>
2020-10-17 20:14:28 -0400

    Checkin #2 via IntelliJ

--- src/main/java/com/siebsen/HelloWorld.java
+++ src/main/java/com/siebsen/HelloWorld.java
@@ -2,6 +2,10 @@ package com.siebsen;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World.....");
+        System.out.println("Hello World.....Checkin #2");
+    }
+
+    public void printSomething(String msg) {
+        System.out.println("some message");
     }
 }
