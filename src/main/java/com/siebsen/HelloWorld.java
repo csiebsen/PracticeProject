26252050ddaa607e548271c3441493d4cc2c72d2
Chuck Siebsen <csiebsen@yahoo.com>
2020-10-17 20:18:39 -0400

    checkin 3

--- src/main/java/com/siebsen/HelloWorld.java
+++ src/main/java/com/siebsen/HelloWorld.java
@@ -3,9 +3,11 @@ package com.siebsen;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World.....Checkin #2");
+        HelloWorld hw = new HelloWorld();
+        hw.printSomething("some message number 3");
     }
 
     public void printSomething(String msg) {
-        System.out.println("some message");
+        System.out.println(msg);
     }
 }
