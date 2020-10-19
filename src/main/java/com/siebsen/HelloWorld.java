a2333b626aae89f91470cc3a01e5879b17c339e6
Chuck Siebsen <csiebsen@yahoo.com>
2020-10-19 14:24:40 -0400

    non mirror change

--- src/main/java/com/siebsen/HelloWorld.java
+++ src/main/java/com/siebsen/HelloWorld.java
@@ -2,7 +2,7 @@ package com.siebsen;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World.....Checkin #2");
+        System.out.println("Hello World.....Non Mirrored");
         HelloWorld hw = new HelloWorld();
         hw.printSomething("some message number 3");
     }
